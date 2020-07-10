bd9e8d24ca22c7e6f01c4e4677913890c4a52de2
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-07-10 00:18:12 -0700

    solved the time index by day

---
+++ code/06_dynamic_topic_modeling.R
@@ -0,0 +1,37 @@
+# Load packages 
+pacman::p_load(keyATM, 
+               here, 
+               tidyverse,
+               quanteda)
+
+# Load files 
+keyATM_docs <- read_rds(here("processed_data", "keyATM_docs.rds"))
+
+my_corpus <- read_rds(here("outputs", "my_corpus.rds"))
+
+# Keywords 
+keywords <- list(anti_chinese = c("wuhanvirus", "chinesevirus", "chinavirus", "wuhancoronavirus", "wuhanpneumonia"))
+
+# Run dynamic topic model 
+future::plan("multiprocess")
+
+tictoc::tic()
+dynamic_out_day <- keyATM(docs = keyATM_docs,    # text input
+                      no_keyword_topics = 2,              # number of topics without keywords
+                      keywords = keywords,       # keywords
+                      model = "dynamic",         # select the model
+                      model_settings = list(time_index = docvars(my_corpus)$index,                                          num_states = 5),
+                      options = list(seed = 250, store_theta = TRUE, thinning = 5))
+tictoc::toc()
+
+# Save 
+write_rds(dynamic_out_day, here("outputs", "dynamic_out_day.rds"))
+
+# Visualize 
+fig_timetrend_day <- plot_timetrend(dynamic_out_day, time_index_label = docvars(my_corpus)$month, xlab = "Date")
+
+# Export 
+
+ggsave(fig_timetrend_day, here("outputs", "dynamic_topic_day.png"))
+
+keyATM::docvars
