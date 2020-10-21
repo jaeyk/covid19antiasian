bec465351ba9468cae14409b6d93a9216408c377
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-10-21 16:31:57 -0700

    added shinyapp sidebar

--- App/app.R
+++ App/app.R
@@ -2,20 +2,32 @@
 # load package 
 
 require("wordcloud2")
+require("vroom")
 require("shiny")
 require("shinydashboard")
 require("colourpicker")
 
 # load data 
 
-df <- read.csv(url("https://github.com/jaeyk/covid19antiasian/raw/master/processed_data/hash_counts.csv"))[,-1]
+df <- vroom::vroom(url("https://github.com/jaeyk/covid19antiasian/raw/master/processed_data/hash_counts.csv"))[,-1]
 
 ui <- fluidPage(
   
-    h1("Word Cloud on the Hashtags of the Tweets related to COVID-19 & Asian|Chinese|Wuhan"),
+    # Application title 
+    titlePanel("Word Cloud on the Hashtags of the Tweets related to COVID-19 & Asian|Chinese|Wuhan"),
   
     h4(tags$a(href = "https://jaeyk.github.io/", "Developer: Jae Yeon Kim")),
             
+    sidebarLayout(
+      
+      # Sidebar with sliders 
+      sidebarPanel(
+        sliderInput("size", 
+                    "Font size:",
+                    min = 1, max = 10,
+                    value = 2)
+      ),
+    
     mainPanel(
           
           wordcloud2Output("cloud"),
@@ -23,13 +35,14 @@ ui <- fluidPage(
         )
     
     )
+)
   
-server <- function(input, output) {
+server <- function(input, output, session) {
   
   output$cloud <- renderWordcloud2({ 
     
     wordcloud2(df, 
-               size = 2.5, 
+               size = input$size, 
                color = "random-dark") 
     
     })
@@ -37,3 +50,4 @@ server <- function(input, output) {
   }
 
 shinyApp(ui = ui, server = server)
+
